c8f162c8031ad13c70ba1cdf9a06c7bb6444faca
Allison <allison.hix86@gmail.com>
2018-07-28 10:24:19 -0500

    New homework

--- CG 4-2/Program.cs
+++ CG 4-2/Program.cs
@@ -6,7 +6,25 @@ namespace CG_4_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // set the array. Here we will use numbers and have 6 in the array
+            // array is like a list of items and you chose the items
+            // we're doing numbers as the items
+            int[] numbers = new int[] { 1, 1, 2, 3, 5, 8 };
+
+            // setting the loop to output all items from array
+            // the i++ adds one to it. so when i = 1 it will pull the 2nd 1 fro the list
+            // the list starts at absolute zero
+           for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadLine();
+
+            
+
+
+
+            
         }
     }
 }
